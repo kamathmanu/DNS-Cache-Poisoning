a27f73868b5f4be5b9912a06e002f1356d6d86ce
kamathmanu <manuriddle@gmail.com>
2020-04-04 13:01:47 -0400

    Part 2 DNS proxy

--- dnsproxy_starter.py
+++ dnsproxy_starter.py
@@ -17,4 +17,30 @@ port = args.port
 dns_port = args.dns_port
 # Flag to indicate if the proxy should spoof responses
 SPOOF = args.spoof_response
+#Default BIND server
+bindIP = "127.0.0.1"
 
+if __name__ == '__main__':
+	#setup a UDP server to get a DNS request over UDP
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+	sock.bind(('', port))
+	while True:
+		#receive a DNS query request from dig
+		print("Waiting for a request")
+		clientRequest, addr = sock.recvfrom(4096)
+		print(addr)
+		print("Received dig request")
+		dnsPacket = DNS(clientRequest)
+		print ("Sending to BIND")
+		sock.sendto(str(dnsPacket), (addr[0], dns_port))
+		#Wait for upstream BIND server's response
+		print ("Waiting for BIND reply")
+		response = sock.recv(4096)
+		response = DNS(response)
+		print "\n***** Packet Received from Remote Server *****"
+		print response.show()
+		print "***** End of Remote Server Packet *****\n"
+		#forward response back to dig
+		print("Forwarding back to dig")
+		sock.sendto(str(response), (addr[0], addr[1]))
+		"Proxy lookup complete\n"
